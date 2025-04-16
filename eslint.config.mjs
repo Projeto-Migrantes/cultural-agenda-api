ab7f2b65fc3c677c7a87c86572cdca62fb201c7e
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-16 01:31:45 -0300

    feat: initialize the Restful API project

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from "@eslint/js";
+import globals from "globals";
+import prettier from "eslint-plugin-prettier";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 2025,
+    },
+    plugins: {
+      js,
+      prettier,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+]);
