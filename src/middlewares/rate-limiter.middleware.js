ccab71b5e6f5ed16f90770a518dc85312bd3a8e2
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-19 17:03:36 -0300

    feat(middeware): add rate limit middleware

---
+++ src/middlewares/rate-limiter.middleware.js
@@ -0,0 +1,9 @@
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 1000,
+  message: "You have made too many requests, please try again later.",
+});
+
+export default limiter;
