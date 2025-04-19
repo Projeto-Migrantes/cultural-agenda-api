877d0651dbe80e62b4915113e1c8e53ca58b0432
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-04-19 17:03:00 -0300

    feat(middeware): add error handling middleware

---
+++ src/middlewares/error-handler.middleware.js
@@ -0,0 +1,13 @@
+const errorHandler = (err, req, res, next) => {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(500).json({ error: "Internal Server Error" });
+};
+
+const notFoundHandler = (req, res) => {
+  res.status(404).json({ error: "Check URL, route not found" });
+};
+
+export { errorHandler, notFoundHandler };
