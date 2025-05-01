e2b7bb4f40eb2824a694e477ade29d885b476352
milenaoliveira07 <imperior5@hotmail.com>
2025-05-01 19:57:55 -0300

    feat: adicionando migration de Migrante

---
+++ src/migrations/20250501225403-create-migrants-table.js
@@ -0,0 +1,88 @@
+export const up = async (queryInterface, Sequelize) => {
+  await queryInterface.createTable('migrants', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    full_name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
+    },
+    date_of_birth: {
+      type: DataTypes.DATE,
+    },
+    phone_number: {
+      type: DataTypes.STRING(15),
+    },
+    crnm: {
+      type: DataTypes.STRING(9),
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    registration_data: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    consent: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    purpose: {
+      type: DataTypes.TEXT,
+    },
+    addresses_id: {
+      type: DataTypes.INTEGER,
+    },
+    address_complement: {
+      type: DataTypes.STRING(50),
+    },
+    address_number: {
+      type: DataTypes.STRING(10),
+    },
+    social_name: {
+      type: DataTypes.STRING(255),
+    },
+    language_preference: {
+      type: DataTypes.STRING(50),
+    },
+    entry_into_brazil: {
+      type: DataTypes.DATE,
+    },
+    migration_reason: {
+      type: DataTypes.STRING(100),
+    },
+    country_of_origin: {
+      type: DataTypes.STRING(50),
+    },
+    gender: {
+      type: DataTypes.STRING(50),
+    },
+    marital_status: {
+      type: DataTypes.STRING(50),
+    },
+    literacy_level: {
+      type: DataTypes.STRING(50),
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  });
+}
+
+export const down = async (queryInterface, Sequelize) =>{
+  await queryInterface.dropTable('migrants');
+}
