3340a5776d5d0c6abd2c14baf836b2618c742322
milenaoliveira07 <imperior5@hotmail.com>
2025-05-04 12:20:23 -0300

    feat: adicionando migration de Migrante v2

--- src/migrations/20250501225403-create-migrants-table.js
+++ src/migrations/20250501225403-create-migrants-table.js
@@ -17,12 +17,16 @@ export const up = async (queryInterface, Sequelize) => {
     },
     date_of_birth: {
       type: DataTypes.DATE,
+      allowNull: false,
     },
     phone_number: {
       type: DataTypes.STRING(15),
+      unique: true,
     },
     crnm: {
       type: DataTypes.STRING(9),
+      unique: true,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING(255),
@@ -31,6 +35,7 @@ export const up = async (queryInterface, Sequelize) => {
     registration_data: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
+      allowNull: false,
     },
     consent: {
       type: DataTypes.BOOLEAN,
@@ -41,36 +46,50 @@ export const up = async (queryInterface, Sequelize) => {
     },
     addresses_id: {
       type: DataTypes.INTEGER,
+        references: {
+          model: Address,
+          key: 'id',
+      },
     },
     address_complement: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     address_number: {
       type: DataTypes.STRING(10),
+      allowNull: false,
     },
     social_name: {
       type: DataTypes.STRING(255),
+      allowNull: true,
     },
     language_preference: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     entry_into_brazil: {
       type: DataTypes.DATE,
+      allowNull: false,
     },
     migration_reason: {
       type: DataTypes.STRING(100),
+      allowNull: true,
     },
     country_of_origin: {
       type: DataTypes.STRING(50),
+      allowNull: false,
     },
     gender: {
       type: DataTypes.STRING(50),
+      allowNull: false,
     },
     marital_status: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     literacy_level: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     created_at: {
       type: DataTypes.DATE,
