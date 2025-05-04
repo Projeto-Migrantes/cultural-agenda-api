eb4638430da8a5afb97bc2d7add4dd8a5fe44e6e
Lucas Araujo <lucasoliveira_0@icloud.com>
2025-05-04 20:20:11 -0300

    feat: atualizar model de address

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,36 +1,36 @@
-module.exports = (sequelize, DataTypes) => {
-    const Address = sequelize.define('Address', {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      postal_code: {
-        type: DataTypes.STRING(10),
-        allowNull: false
-      },
-      street: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-      },
-      neighborhood: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-      },
-      city: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-      },
-      state: {
-        type: DataTypes.STRING(50),
-        allowNull: false
-      },
-      
-    }, {
-      tableName: 'addresses',
-    });
+import sequelize from '../config/database.config.js';
 
-  
-    return Address;
-  };
-  
+import { DataTypes } from 'sequelize';
+
+const Address = sequelize.define('Address', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  postal_code: {
+    type: DataTypes.STRING(10),
+    allowNull: false
+  },
+  street: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  neighborhood: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  city: {
+    type: DataTypes.STRING(255),
+    allowNull: false
+  },
+  state: {
+    type: DataTypes.STRING(50),
+    allowNull: false
+  }
+}, {
+  tableName: 'addresses',
+  timestamps: true 
+});
+
+export default Address;
