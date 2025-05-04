a0805882f3c9c36b0460909045ad38861a271d27
milenaoliveira07 <imperior5@hotmail.com>
2025-05-04 12:14:14 -0300

    feat: adicionando Model Migrante v2

--- src/models/Migrant.js
+++ src/models/Migrant.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import connection from "../config/database.js";
+import Address from "./Address.js";
 
 const Migrant = connection.define(
   "Migrant",
@@ -21,12 +22,16 @@ const Migrant = connection.define(
     },
     date_of_birth: {
       type: DataTypes.DATE,
+      allowNull: false,
     },
     phone_number: {
       type: DataTypes.STRING(15),
+      unique: true,
     },
     crnm: {
       type: DataTypes.STRING(9),
+      unique: true,
+      allowNull: false,
     },
     password: {
       type: DataTypes.STRING(255),
@@ -35,6 +40,7 @@ const Migrant = connection.define(
     registration_data: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
+      allowNull: false,
     },
     consent: {
       type: DataTypes.BOOLEAN,
@@ -45,44 +51,50 @@ const Migrant = connection.define(
     },
     addresses_id: {
       type: DataTypes.INTEGER,
+        references: {
+          model: Address,
+          key: 'id',
+      },
     },
     address_complement: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     address_number: {
       type: DataTypes.STRING(10),
+      allowNull: false,
     },
     social_name: {
       type: DataTypes.STRING(255),
+      allowNull: true,
     },
     language_preference: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     entry_into_brazil: {
       type: DataTypes.DATE,
+      allowNull: false,
     },
     migration_reason: {
       type: DataTypes.STRING(100),
+      allowNull: true,
     },
     country_of_origin: {
       type: DataTypes.STRING(50),
+      allowNull: false,
     },
     gender: {
       type: DataTypes.STRING(50),
+      allowNull: false,
     },
     marital_status: {
       type: DataTypes.STRING(50),
+      allowNull: true,
     },
     literacy_level: {
       type: DataTypes.STRING(50),
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW,
+      allowNull: true,
     },
   },
   {
