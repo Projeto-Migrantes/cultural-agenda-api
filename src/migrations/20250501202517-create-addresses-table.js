4e04407bc49e612ee4951b22d5be0de6ec31b65c
Lucas Araujo <lucasoliveira_0@icloud.com>
2025-05-04 20:08:59 -0300

    fix: ajusta migration de addresses

--- src/migrations/20250501202517-create-addresses-table.js
+++ src/migrations/20250501202517-create-addresses-table.js
@@ -1,46 +1,42 @@
-
 export const up = async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('addresses', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      postal_code: {
-        type: Sequelize.STRING(10),
-        allowNull: false
-      },
-      street: {
-        type: Sequelize.STRING(255),
-        allowNull: false
-      },
-      neighborhood: {
-        type: Sequelize.STRING(255),
-        allowNull: false
-      },
-      city: {
-        type: Sequelize.STRING(255),
-        allowNull: false
-      },
-      state: {
-        type: Sequelize.STRING(50),
-        allowNull: false
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW
-      }
-    });
-  }
+  await queryInterface.createTable('addresses', {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    },
+    postal_code: {
+      type: Sequelize.STRING(10),
+      allowNull: false
+    },
+    street: {
+      type: Sequelize.STRING(255),
+      allowNull: false
+    },
+    neighborhood: {
+      type: Sequelize.STRING(255),
+      allowNull: false
+    },
+    city: {
+      type: Sequelize.STRING(255),
+      allowNull: false
+    },
+    state: {
+      type: Sequelize.STRING(50),
+      allowNull: false
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false
+    }
+  });
+};
 
-  export const down = async (queryInterface, Sequelize) =>{
-    await queryInterface.dropTable('addresses');
-  }
-
+export const down = async (queryInterface, Sequelize) => {
+  await queryInterface.dropTable('addresses');
+};
