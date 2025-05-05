181b26d5ee4dd9228cd584bc0290bed5ea45e820
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-05 00:40:45 -0300

    refactor(model): refactor the migrant model

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,36 +1,44 @@
-import sequelize from '../config/database.config.js';
+import sequelize from "../config/database.config.js";
 
-import { DataTypes } from 'sequelize';
+import { DataTypes } from "sequelize";
 
-const Address = sequelize.define('Address', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
+const Address = sequelize.define(
+  "Address",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    postal_code: {
+      type: DataTypes.STRING(10),
+      allowNull: false,
+    },
+    street: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    neighborhood: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
   },
-  postal_code: {
-    type: DataTypes.STRING(10),
-    allowNull: false
+  {
+    tableName: "addresses",
+    timestamps: true,
   },
-  street: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  neighborhood: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  city: {
-    type: DataTypes.STRING(255),
-    allowNull: false
-  },
-  state: {
-    type: DataTypes.STRING(50),
-    allowNull: false
-  }
-}, {
-  tableName: 'addresses',
-  timestamps: true 
+);
+
+Address.hasMany(Migrant, {
+  foreignKey: "addresses_id",
+  as: "address",
 });
-
 export default Address;
